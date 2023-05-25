973eb9d2c05471710e9fccd60d23fce4be1a04f2
yashwant khare <106534840+Yashwant23@users.noreply.github.com>
2023-05-25 17:34:39 +0530

    Backend and Frontend push to Git

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import classes from './footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className={classes.wrapper}>
+        <div className={classes.col}>
+          <h2>About the App</h2>
+          <p>It is an blog app created using MERN stack.
+          </p>
+        </div>
+        <div className={classes.col}>
+          <h2>Contacts</h2>
+          <span>Phone: +123 456 789</span>
+          <span>GitHub: Yashwant23</span>
+        </div>
+        <div className={classes.col}>
+          <h2>Location</h2>
+          <span>Continent: Asia</span>
+          <span>Country: India</span>
+          <span>Current Location: Jhansi</span>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
